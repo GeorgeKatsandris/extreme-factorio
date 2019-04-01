24c77e7a1a3f902e3249c9fbae63e138db93d627
GeorgeKatsandris <george.katsandris@yandex.com>
2019-04-01 22:35:48 +0300

    implemented proper user input

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,30 @@
 int main()
 {
 	Item::initialise_itemList("itemdb.txt");
-	auto test = Item::itemList.at("processing unit").calculate_production_requirements(6);
 	
-	cout << endl;
-	print_map(test);
+	string itemName;
+	float requiredProductionRate;
+	cout << "Please enter the item you want to mass-produce:" << endl << "    ";
+
+	getline(cin,itemName);
+	
+	if (Item::itemList.count(itemName) != 0)
+	{
+		cout << "how many per minute?" << endl << "    ";
+		cin >> requiredProductionRate;
+		
+		auto totalMats = Item::itemList.at(itemName).calculate_production_requirements(requiredProductionRate);
+		cout << endl;
+		
+		cout << "Total production rate of each material:" << endl;
+		for (auto it = totalMats.cbegin(); it != totalMats.cend(); it++)
+		{
+			cout << it->first << " @" << it->second <<"/min" << endl;
+		}
+		
+	}
+	else
+		cout << "Invalid item name." << endl;
 	
 	system("pause");
 	return 0;
