d30f735f5fe47ef1f4f1c84f0c46d77e26e1e346
GeorgeKatsandris <george.katsandris@yandex.com>
2019-04-01 23:16:50 +0300

    made program output prettier

--- src/main.cpp
+++ src/main.cpp
@@ -6,28 +6,31 @@ int main()
 	
 	string itemName;
 	float requiredProductionRate;
-	cout << "Please enter the item you want to mass-produce:" << endl << "    ";
+	cout << "Please enter the item you want to mass-produce:" << endl << "    >";
 
-	getline(cin,itemName);
+	//getline(cin,itemName);
+	itemName = "processing unit";
 	
 	if (Item::itemList.count(itemName) != 0)
 	{
-		cout << "how many per minute?" << endl << "    ";
-		cin >> requiredProductionRate;
+		cout << "how many per minute?" << endl << "    >";
+		//cin >> requiredProductionRate;
+		requiredProductionRate = 6.5;
+		cout << endl;
 		
+		cout << "=== Production rate requirement analysis: ===" << endl;
 		auto totalMats = Item::itemList.at(itemName).calculate_production_requirements(requiredProductionRate);
 		cout << endl;
 		
-		cout << "Total production rate of each material:" << endl;
+		cout << "=== Total required production rate of each material: ===" << endl;
 		for (auto it = totalMats.cbegin(); it != totalMats.cend(); it++)
 		{
 			cout << it->first << " @" << it->second <<"/min" << endl;
 		}
-		
 	}
 	else
 		cout << "Invalid item name." << endl;
 	
-	system("pause");
+	//system("pause");
 	return 0;
 }
